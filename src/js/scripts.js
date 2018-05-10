ec298b16251f8e4dac9212bc2523ccf9be8f35f3
George Tourtsinakis <george@besmartbesimple.com>
2018-05-10 12:25:02 +0300

    Remove mustache and bootstrap - Some navigation menu design

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,16 @@
 //Browserify syntax
 global.jQuery = require('jquery'); //global function makes it top-level scope object (window)
-bootstrap = require('bootstrap');
-mustache = require('mustache');
+//bootstrap = require('bootstrap');
+//mustache = require('mustache');
 
 
 //Get data from json and show them to template with mustache
-jQuery(document).ready(function($){
-  var jsonData =  $.getJSON('data.json',function(){
-
-  }).done(function(data){
-    var template = $('#template').html();
-    var showTemplate = mustache.render(template,data)//where the template will be created, what data
-    $('#gallery').html(showTemplate);//where to show the template we created
-  });
-});
+// jQuery(document).ready(function($){
+//   var jsonData =  $.getJSON('data.json',function(){
+//
+//   }).done(function(data){
+//     var template = $('#template').html();
+//     var showTemplate = mustache.render(template,data)//where the template will be created, what data
+//     $('#gallery').html(showTemplate);//where to show the template we created
+//   });
+// });
